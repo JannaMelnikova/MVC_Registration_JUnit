961df24ba7ebd413fdd15b0a09a66cebd6c2902d
JannaMelnikova <jannet87@list.ru>
2024-09-04 22:52:29 +0300

    add test by UserService

--- src/test/java/org/example/mvc_registration/service/UserServiceTest.java
+++ src/test/java/org/example/mvc_registration/service/UserServiceTest.java
@@ -11,9 +11,12 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.Mockito.when;
 
 
@@ -70,7 +73,58 @@ class UserServiceTest {
         assertEquals("Sidorov", userResult.getName());
         assertEquals("ghffhg", userResult.getPassword());
         assertEquals("jannet87@list.ru", userResult.getEmail());
+    }
 
+    @Test
+    void getUserById() {
+        User user = new User();
+        user.setId(1);
+        user.setName("Sidorov");
+        user.setPassword("ghffhg");
+        user.setEmail("jannet87@list.ru");
+        // прописываем повидение для репозитория если у него будет вызван метод findByName с id=1
+        when(userRepository.findById(user.getId())).thenReturn(Optional.of(user));
+        /** Главная строка для теста сервиса и его метода getUserById */
+        //получаем результат метода getUserById из service
+        Optional<User> userId = userService.getUserById(user.getId());
+        //проверяем результат с тем что ожидали получить
+        assertEquals(user, userId.get());
+        assertEquals(1, userId.get().getId());
 
     }
+
+    @Test
+    void getUserByEmail() {
+        User user = new User();
+        user.setName("Sidorov");
+        user.setPassword("ghffhg");
+        user.setEmail("jannet87@list.ru");
+        // прописываем повидение для репозитория если у него будет вызван метод findByName с email=jannet87@list.ru
+        when(userRepository.findByEmail(user.getEmail())).thenReturn(Optional.of(user));
+        /** Главная строка для теста сервиса и его метода getUserByEmail */
+        //получаем результат метода getUserByEmail из service
+        Optional<User> userEmail = userService.getUserByEmail(user.getEmail());
+        assertEquals(user, userEmail.get());
+        assertEquals("jannet87@list.ru", userEmail.get().getEmail());
+    }
+
+    @Test
+    void findAll() {
+        User user = new User();
+        user.setName("Sidorov");
+        user.setPassword("ghffhg");
+        user.setEmail("jannet87@list.ru");
+        List<User> userList = Collections.singletonList(user);
+
+        when(userRepository.findAll()).thenReturn(userList);
+
+        List<User> userResultList = userService.findAll();
+        assertEquals(1, userResultList.size());
+        User userResult = userResultList.get(0);
+
+        assertEquals("Sidorov", userResultList.get(0).getName());
+        assertEquals("ghffhg", userResultList.get(0).getPassword());
+        assertEquals("jannet87@list.ru", userResultList.get(0).getEmail());
+    }
+
 }
